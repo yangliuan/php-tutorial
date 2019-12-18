f24e9e32ee8885e4063090e9788db14a489c54b9
yangliuan <yangliuancn@foxmail.com>
2019-12-18 22:23:19 +0800

    修改

--- process/pcntl/fork.php
+++ process/pcntl/fork.php
@@ -2,24 +2,24 @@
 
 //获取当期那进程id
 $pid = posix_getpid();
-echo "my pid: {$pid}\n";
+echo "my pid: {$pid}", PHP_EOL;
 $childNum = 10;
 for ($i = 1; $i <= 10; ++$i) {
     //fork一个进程
     $pid = pcntl_fork();
     //创建失败
     if (-1 === $pid) {
-        echo "failt to fork!\n";
+        echo "failt to fork!", PHP_EOL;
         exit;
     } elseif ($pid > 0) {
         //parent code
-        echo "fork the {$i}th child,pid:{$pid}\n";
+        echo "fork the {$i}th child,pid:{$pid}", PHP_EOL;
     } else {
         //当前进程pid
         $mypid = posix_getpid();
         //父进程pid
         $parentpid = posix_getppid();
-        echo "I'm the {$i}th child and my pid:{$mypid},parentpid:{$parentpid}\n";
+        echo "I'm the {$i}th child and my pid:{$mypid},parentpid:{$parentpid}", PHP_EOL;
         sleep(5);
         exit;
     }
