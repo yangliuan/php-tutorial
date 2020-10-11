332dcaca54ba10fa33fe23f7b4417caef5292f2f
yangliuan <yangliuancn@foxmail.com>
2020-10-11 14:22:57 +0800

    demo

--- function-reference/file-system/metadata/info.php
+++ function-reference/file-system/metadata/info.php
@@ -1,6 +1,13 @@
 <?php
-$filePath = '/home/yangliuan/Pictures/Wallpapers/WriteCode.png';
+$filePath = 'demo.txt';
 
+echo '<h1>修改前</h1>';
+echo 'filectime', '创建时间', date('Y-m-d H:i:s', filectime($filePath)), PHP_EOL;
+echo 'fileatime', '访问时间', date('Y-m-d H:i:s', fileatime($filePath)), PHP_EOL;
+echo 'filemtime', '修改时间', date('Y-m-d H:i:s', filemtime($filePath)), PHP_EOL;
+
+touch($filePath, strtotime('2020-01-01 00:00:01'), time());
+echo '<h1>修改后</h1>';
 echo 'filectime', '创建时间', date('Y-m-d H:i:s', filectime($filePath)), PHP_EOL;
 echo 'fileatime', '访问时间', date('Y-m-d H:i:s', fileatime($filePath)), PHP_EOL;
 echo 'filemtime', '修改时间', date('Y-m-d H:i:s', filemtime($filePath)), PHP_EOL;
